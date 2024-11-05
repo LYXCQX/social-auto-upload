cc2449f4b5cad776fd5c420e550dbf11fc761715
41525 <415256473@qq.com>
2024-11-05 23:09:20 +0800

    抛出异常

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from loguru import logger
-
-# 定义公共日志输出模板
-logger.add("info.log", format="{time} {level} {message}", level="INFO")
-
-# 定义异常日志输出模板
-logger.add("error.log", format="{time} {level} {message} {exception}", level="ERROR")
